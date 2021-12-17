fed92ced59c5428ed1e91f8f4381d929b5d79864
Varik Valefor <varikvalefor@aol.com>
2021-12-16 19:28:51 -0500

    FEATURE: Support for line numbering is added.  This change implies an overhaul of the inner workings of the program.

--- app/shat.hs
+++ app/shat.hs
@@ -4,16 +4,29 @@ import System.Directory;
 import System.Environment;
 import System.IO;
 import Data.List;
+import Text.Read;
+import Data.Maybe;
 import Data.List.Split;
+
+-- | The main 'EdData' variable contains the buffer and the current line
+-- of the program.
+data EdData = EdData {
+  -- | Where @k@ is the "main" 'EdData' variable of the program,
+  -- @currentLine k@ is the current line.
+  currentLine :: Int,
+  -- | Where @k@ is the "main" 'EdData' variable of the program,
+  -- @stk k@ is the text buffer.
+  --
+  -- "@stk@" is derived from "beef", which is derived from "buffer".
+  stk :: [String]
+};
 
 main :: IO ();
 main = getArgs >>= \a ->
   if a == []
-    then edFunction a
-    else doesFileExist (a !! 0) >>= \fileExists ->
-      if fileExists
-        then readFile (a !! 0) >>= edFunction . lines
-        else writeFile (a !! 0) "" >> main;
+    then edFunction $ wBuf []
+    else readFile (a !! 0) >>= edFunction . wBuf . lines
+  where wBuf beefer = EdData {currentLine = 1, stk = beefer};
 
 -- | @parseNums n b@ is an 'Int'-based list of the line numbers which
 -- are specified by the the ed(1)-compliant @n@.
@@ -56,64 +69,73 @@ insertAt n x xs = take g xs ++ x ++ drop g xs
 edPrintLine :: Int
             -- ^ The index of the line which is written to the
             -- standard output
-            -> [String]
-            -- ^ The buffer
-            -> IO [String];
-edPrintLine n buf = (putStrLn $ buf !! (n - 1)) >> return buf;
+            -> EdData
+            -- ^ The program's main text thing
+            -> IO EdData;
+edPrintLine n e = (putStrLn $ stk e !! (n - 1)) >> return e;
 
 -- | @edInsert n buffer@ inserts some lines which are read from the
 -- standard input immediately before the @n@th line of @buffer@,
 -- returning the resulting buffer.
 --
 -- Lines are 1-indexed.
-edInsert n buf = isEOF >>= \ a ->
-  if a
-    then return buf
-    else getLine >>= \ x -> edInsert (n + 1) $ insertAt n [x] buf;
+edInsert :: Int
+         -- ^ The index of the line which follows the inserted stuff
+         -> EdData
+         -> IO EdData;
+edInsert n e = isEOF >>= \ a -> if a
+  then return e
+  else getLine >>= \ x ->
+    if x == "."
+      then return e
+      edInsert (n + 1) $ e {stk = insertAt n [x] (stk e)};
 
 -- | @edWrite buffer finename@ writes the @buffer@ to the file whose
 -- path is @finename@.  The name of the fille need not be fine.
-edWrite :: [String]
+edWrite :: EdData
         -- ^ The buffer
         -> String
         -- ^ The file path
-        -> IO [String];
-edWrite buf fn = writeFile fn conkd >> return buf
-  where conkd = foldr (++) [] $ intersperse "\n" buf;
+        -> IO EdData;
+edWrite e fn = writeFile fn ((++ "\n") $ unlines $ stk e) >> return e;
 
--- | @edDel n buffer@ is a @buffer@ which lacks line @n@, where lines
+-- | @edDel n dt@ is a @dt@ whose buffer lacks line @n@, where lines
 -- are 1-indexed.
 edDel :: Int
       -- ^ The index of the line which is removed
-      -> [String]
+      -> EdData
       -- ^ The buffer from which a line is removed
-      -> IO [String];
-edDel n buf = return $ take (n - 1) buf ++ drop n buf;
+      -> IO EdData;
+edDel n e = return e {stk = take (n - 1) (stk e) ++ drop n (stk e)};
 
 -- | @edFunction@ is the "meat and potatoes" of @shat@.
 --
 -- @edFunction buffer@ reads a command from the standard input and
 -- operates on the @buffer@.
-edFunction :: [String]
+edFunction :: EdData
            -- ^ The buffer
            -> IO ();
-edFunction buf = getLine >>= detFun >>= edFunction
+edFunction eddy = getLine >>= detFun >>= edFunction
   where
   detFun cmd
     | length cmd == 0 = err
-    | last cmd == 'p' = mapM_ (flip edPrintLine buf) k >> return buf
+    | last cmd == 'p' = mapM_ (\m -> edPrintLine m eddy)
+      k >> return eddy
     | last cmd == 'n' = mapM_ (\m -> putStr (show m ++ "\t") >>
-      edPrintLine m buf) k >> return buf
-    | cmd == "i" = edInsert 1 buf
-    | last cmd == 'p' = edPrintLine n buf
-    | last cmd == 'i' = edInsert n buf
-    | last cmd == 'a' = edInsert (n + 1) buf
-    | head cmd == 'w' = edWrite buf $ unwords $ drop 1 $ words cmd
-    | last cmd == 'd' = edDel n buf
+      edPrintLine m eddy) k >> return eddy
+    | last cmd == 'p' = edPrintLine n eddy
+    | last cmd == 'i' = edInsert n eddy
+    | head cmd == 'w' = edWrite (eddy) $ drop 2 cmd
+    | last cmd == 'd' = edDel n eddy
     | last cmd == 'q' = exitSuccess
-    | last cmd == 'c' = edDel n buf >>= edInsert n
+    | last cmd == 'c' = edDel n eddy >>= edInsert n
+    | isJust $ (readMaybe cmd :: Maybe Int) =
+      return eddy {currentLine = read cmd}
     | otherwise = err
-    where
-    n = read $ init cmd
-    k = genRange $ parseNums cmd buf
-  err = putStrLn "?" >> return buf;
+    where n | not $ head cmd `elem` ['a'..'z'] = read $ init cmd :: Int
+              -- \^ Parsing commands as numbers is a pretty stupid
+              -- idea.  As such, commands are not parsed as numbers.
+            | otherwise = currentLine eddy
+          k = genRange $ parseNums cmd $ stk eddy
+          party k = init k ++ [last k ++ "\n"]
+  err = putStrLn "?" >> return eddy;
